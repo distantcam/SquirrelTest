045d711b4ca94ab66466c937fd4da0f632782cb1
Cameron MacFarland <distantcam@gmail.com>
2016-10-25 11:58:28 +0800

    Initial version

---
+++ src/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Squirrel;
+
+namespace MyApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Task.Run(async () =>
+            {
+                using (var mgr = new UpdateManager(@"I:\src\MyApp\Releases"))
+                {
+                    await mgr.UpdateApp();
+                }
+            });
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
